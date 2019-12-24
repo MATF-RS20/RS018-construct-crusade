bf3333af46576bc75c456722f90b69f5c02c1bf0
AleksandarTheBoulderRankovic <mi16111@alas.matf.bg.ac.rs>
2019-12-25 00:56:38 +0100

    week 3 - robot walking animation, view move that follows the player

--- Construct_crusade.cpp
+++ Construct_crusade.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 using namespace sf;
 double VEL = 1;
@@ -38,6 +39,8 @@ public:
 
     PlayerClass(Sprite sprite, double x_pos, double y_pos) : GameObject(x_pos, y_pos), sprite_(sprite){
         sprite_.setPosition(GameObject::x_pos_, GameObject::y_pos_);
+        scale_ = 4;
+        sprite_.setScale(scale_, scale_);
     }
 
     //gain velocity if a button is pressed
@@ -75,6 +78,7 @@ public:
 
 
     Sprite sprite_;
+    double scale_;
     double x_vel_;
     double y_vel_;
 };
@@ -84,11 +88,17 @@ int main(){
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "SFML window");
 
+    //define a view
+    sf::View view(sf::FloatRect(0.0, 0.0, window_width, window_height));
+
+    //activate it
+    window.setView(view);
+
     //the construct - our protagonist
     Texture constructTex;
-    constructTex.loadFromFile("assets/images/roboto100x100.png");
+    constructTex.loadFromFile("assets/images/walkingAnimation10.png");
     Sprite constructSprite(constructTex);
-    constructSprite.setTextureRect(IntRect(16, 0, 75, 99));
+    constructSprite.setTextureRect(IntRect(125, 50, 25, 25));
     bool player_right, player_left, player_up, player_down;
 
     PlayerClass player(constructSprite, 0, 0);
@@ -101,9 +111,23 @@ int main(){
     backgroundSprite.getTextureRect();
     backgroundSprite.scale(window_width/backgroundSprite.getLocalBounds().width, window_height/backgroundSprite.getLocalBounds().height);
 
+    //make a vector that contains rectangle information that corelates to picture frames we want to animate
+    std::vector<IntRect> rectangles;
+    rectangles.push_back(IntRect(125, 50, 25, 25));
+    rectangles.push_back(IntRect(150, 50, 25, 25));
+    rectangles.push_back(IntRect(175, 50, 25, 25));
+    rectangles.push_back(IntRect(200, 50, 25, 25));
+    rectangles.push_back(IntRect(225, 50, 25, 25));
+    rectangles.push_back(IntRect(250, 50, 25, 25));
+    int rectangles_index = 0;
+    int time;
+    //delt_time - duration of a frame
+    int delta_time = 200;
+    //helps us determin if our frame should be changed - it marks the time when a frame is first used
+    int current_frame_time = 0;
+
     //platform initialization
     std::vector<PlatformClass> platforms;
-
     Texture platformTex;
     platformTex.loadFromFile("assets/images/tileset.png");
 
@@ -128,6 +152,8 @@ int main(){
 
     platforms.push_back(platform_3);
 
+    Clock gameClock;
+
 
    //start the main loop
     while (window.isOpen())
@@ -139,19 +165,31 @@ int main(){
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::Resized)
+                {
+                // update the view to the new size of the window
+                sf::FloatRect visibleArea(0.0, 0.0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+                }
 
         }
-
         window.clear();
 
         window.draw(backgroundSprite);
 
         //detect an arrow key press - i might find a better way for this logic
-        if(Keyboard::isKeyPressed(Keyboard::Right))
+        if(Keyboard::isKeyPressed(Keyboard::Right)){
             player_right = true;
+            //move the view with the player
+            view.move(1.f, 0.f);
+            window.setView(view);
+        }
 
-        if(Keyboard::isKeyPressed(Keyboard::Left))
+        if(Keyboard::isKeyPressed(Keyboard::Left)){
             player_left = true;
+            view.move(-1.f, 0.f);
+            window.setView(view);
+        }
 
         if(Keyboard::isKeyPressed(Keyboard::Up))
             player_up = true;
@@ -177,7 +215,22 @@ int main(){
         //check for collision and move
         player.collision(platforms);
 
+        time = gameClock.getElapsedTime().asMilliseconds();
+
+        //update rectangle_index if its time for the next frame
+        if(current_frame_time+delta_time < time){
+            current_frame_time = time;
+            if(rectangles_index == 5){
+                rectangles_index = 0;
+                current_frame_time = 0;
+                rectangles_index--;
+                gameClock.restart();
+            }
+            rectangles_index++;
+        }
+
         //draw construct
+        player.sprite_.setTextureRect(rectangles[rectangles_index]);
         window.draw(player.sprite_);
 
         //draw platforms
